5a9dab0b169e201551d499026f5061ab7660460f
Aditya <adityapratamax11@gmail.com>
2022-08-29 07:43:02 +0700

    add reanimated & navigator

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-// 1. import `NativeBaseProvider` component
 import { NativeBaseProvider, Text, Box } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   // 2. Use at the root of your app
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+          <Text>Open up App.js to start working on your app!</Text>
+        </Box>
+      </NativeBaseProvider>
+    </NavigationContainer>
+
   );
 }
--- android/app/src/main/java/com/atomicmobile/MainActivity.java
+++ android/app/src/main/java/com/atomicmobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.atomicmobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "AtomicMobile";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
@@ -37,4 +43,5 @@ public class MainActivity extends ReactActivity {
       return reactRootView;
     }
   }
+  
 }
